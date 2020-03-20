31b63e61ff32d2bea2e6b701de6c2bbec0419014
svakam <vikramakam@hotmail.com>
2020-03-20 16:19:02 -0700

    first commit + gitignore updated

---
+++ src/main/java/com/vik/covid19vik/Covid19vikApplication.java
@@ -0,0 +1,13 @@
+package com.vik.covid19vik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19vikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19vikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vik/covid19vik/Covid19vikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vik.covid19vik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19vikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
