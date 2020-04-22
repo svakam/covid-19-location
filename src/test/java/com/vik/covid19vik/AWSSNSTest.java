ed48525e1150c83755c071c2df5fce41baff6f5a
svakam <vikramakam@hotmail.com>
2020-04-22 02:49:38 -0700

    all tests passing for sns, message being received on phone

--- src/test/java/com/vik/covid19vik/AWSSNSTest.java
+++ src/test/java/com/vik/covid19vik/AWSSNSTest.java
@@ -3,14 +3,11 @@ package com.vik.covid19vik;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sns.SnsAsyncClient;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.SnsClientBuilder;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
 import software.amazon.awssdk.services.sns.model.SubscribeRequest;
 
 public class AWSSNSTest {
@@ -23,17 +20,43 @@ public class AWSSNSTest {
         client.close();
     }
 
+    @Test
+    void testCredentials() {
+        Region region = Region.US_WEST_2;
+        SnsClient client = SnsClient.builder()
+                .region(region)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+        System.out.println(client);
+    }
+
     // client builder
     @Test
     void testClientBuilder() {
-        SnsClient client = SnsClient.create();
-        assertTrue(client.toString().contains("software.amazon.awssdk.services.sns.DefaultSnsClient@"));
+        SnsClient client = SnsClient.builder()
+                .region(Region.US_WEST_2)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+
+        // subscribe request
         String arn = "arn:aws:sns:us-west-2:404079974108:CovidLocatorSNS";
-        final SubscribeRequest sr = SubscribeRequest.builder().topicArn(arn).protocol("sms").endpoint("4258923077").build();
+        final SubscribeRequest sr = SubscribeRequest.builder()
+                .topicArn(arn)
+                .protocol("sms")
+//                .endpoint("enter phone # here")
+                .build();
         client.subscribe(sr);
-        assertEquals("4258923077", sr.endpoint());
+//        assertEquals("enter phone # here", sr.endpoint());
         assertEquals("sms", sr.protocol());
         assertEquals(arn, sr.topicArn());
+
+        final String msg = "For King County, Washington, USA on 04/21/20:\nNew confirmed cases: 100\nTotal confirmed cases: 4000\nNew fatalities: 50\nTotal fatalities: 100\nNew recoveries: N/A\nTotal recoveries: N/A";
+        final PublishRequest pr = PublishRequest.builder()
+                .topicArn(arn)
+                .message(msg)
+                .build();
+        final PublishResponse publishResponse = client.publish(pr);
+
         client.close();
     }
 }
