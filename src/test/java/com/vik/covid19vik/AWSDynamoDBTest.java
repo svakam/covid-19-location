d5133f1db007f1c38f12f76a108c4ee2b85d4f59
svakam <vikramakam@hotmail.com>
2020-04-24 13:32:08 -0700

    able to find table - had to add iam permission for dynamodb

--- src/test/java/com/vik/covid19vik/AWSDynamoDBTest.java
+++ src/test/java/com/vik/covid19vik/AWSDynamoDBTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +23,13 @@ public class AWSDynamoDBTest {
                 .region(Region.US_WEST_2)
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
+
+        ListTablesResponse response = null;
+        ListTablesRequest request = ListTablesRequest.builder().build();
+        response = client.listTables(request);
+        System.out.println("table names = " + response.tableNames());
+        System.out.println("has table names = " + response.hasTableNames());
     }
+
+
 }
