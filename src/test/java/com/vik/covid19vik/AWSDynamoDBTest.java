217037a0ca99e3edfecacf2cc7eead65a8868dd3
svakam <vikramakam@hotmail.com>
2020-04-24 13:16:17 -0700

    snsadd template needed to be renamed, was causing error and canvas to break

--- src/test/java/com/vik/covid19vik/AWSDynamoDBTest.java
+++ src/test/java/com/vik/covid19vik/AWSDynamoDBTest.java
@@ -1,12 +1,25 @@
 package com.vik.covid19vik;
 
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AWSDynamoDBTest {
     @Test
     void testClient() {
         DynamoDbClient client = DynamoDbClient.create();
         System.out.println(client);
+        assertTrue(client.toString().contains("software.amazon.awssdk.services.dynamodb.DefaultDynamoDbClient@"));
+    }
+
+    @Test
+    void testGetTable() {
+        DynamoDbClient client = DynamoDbClient.builder()
+                .region(Region.US_WEST_2)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
     }
 }
